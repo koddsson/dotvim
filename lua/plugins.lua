449f38c70bff0e79a86f42429b4fca618068a580
Kristján Oddsson <koddsson@gmail.com>
2021-11-11 21:17:51 +0000

    Clean up plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,8 @@
 -- Install vim-plug
-local data_dir = vim.fn.has('nvim') and vim.fn.stdpath('data') .. '/site' or '~/.vim'
-if vim.fn.empty(vim.fn.glob(data_dir .. '/autoload/plug.vim')) == 1 then
-  vim.cmd('silent execute \'!curl -fLo ' .. data_dir .. '/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim\'')
- vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+local data_dir = vim.fn.stdpath('data') .. '/site/autoload/plug.vim'
+if vim.fn.empty(vim.fn.glob(data_dir)) == 1 then
+  vim.cmd('silent execute \'!curl -fLo ' .. data_dir .. ' --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim\'')
+ vim.cmd 'autocmd VimEnter * PlugInstall --sync | source $MYVIMRC'
 end
 
 -- Set up Plug function 
@@ -16,30 +16,22 @@ vim.call('plug#begin', plug_path)
 -- Add LSP support
 Plug 'neovim/nvim-lspconfig'
 
--- The best way to find files
+-- The best way to find files.
+--
 -- PlugInstall and PlugUpdate will clone fzf in ~/.fzf and run install script
+-- Both options are optional. You don't have to install fzf in ~/.fzf and you
+-- don't have to run install script if you use fzf only in Vim.
 Plug('junegunn/fzf', {dir = '~/.fzf', ['do'] = './install --all' })
--- Both options are optional. You don't have to install fzf in ~/.fzf
--- and you don't have to run install script if you use fzf only in Vim.
-
--- for `:Ag`
 Plug 'junegunn/fzf.vim'
 
 -- The cool line at the bottom
 Plug 'itchyny/lightline.vim'
-
--- For node plugings
-Plug('neovim/node-host', { ['do'] = 'npm install -g neovim' })
 
 -- Solarized
 Plug 'altercation/vim-colors-solarized'
 
 -- cs'" to replace ' with ", ds' to remove '
 Plug 'tpope/vim-surround'
-
--- For repeating plugin commands
--- XXX: Am I using this?
-Plug 'tpope/vim-repeat'
 
 -- `g c` to comment and uncomment lines
 Plug 'tpope/vim-commentary'
