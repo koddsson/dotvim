f9ab76627fff3e4712967fe80835e6c62c2c84f4
Kristján Oddsson <koddsson@gmail.com>
2021-11-11 21:19:33 +0000

    Fix `vim-cmp` and move LSP configuration to own file

--- init.lua
+++ init.lua
@@ -8,19 +8,8 @@ require('plugins')
 require('options')
 require('maps')
 require('autocommands')
+require('lsp')
 
--- Set up LSP servers
-require'lspconfig'.tsserver.setup{}
-require'lspconfig'.eslint.setup{}
-require'lspconfig'.html.setup{}
-
--- TODO: I want deno only to be configured when we're in a deno project. Maybe
--- even turn off the tsserver one.
--- require'lspconfig'.denols.setup{
---   root_dir = root_pattern("mod.ts")
--- }
-
--- TODO: Enable once I figure out how to run nvim nightly
 -- Configure treesitter
 local ts = require 'nvim-treesitter.configs'
 ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
---
+++ lua/lsp.lua
@@ -0,0 +1,88 @@
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+    end,
+  },
+  mapping = {
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- Set up LSP servers
+
+-- HTML
+require'lspconfig'.html.setup{
+  capabilities = capabilities
+}
+
+-- CSS
+require'lspconfig'.cssls.setup{
+  capabilities = capabilities
+}
+require'lspconfig'.stylelint_lsp.setup{
+  capabilities = capabilities
+}
+
+-- TypeScript and JavaScript
+require'lspconfig'.tsserver.setup{
+  capabilities = capabilities
+}
+require'lspconfig'.eslint.setup{
+  capabilities = capabilities,
+  on_attach = function(client)
+    client.resolved_capabilities.document_formatting = true
+  end,
+  flags = {
+    debounce_text_changes = 150,
+  }
+}
+
+-- TODO: I want deno only to be configured when we're in a deno project. Maybe
+-- even turn off the tsserver one.
+-- require'lspconfig'.denols.setup{
+--   capabilities = capabilities
+--   root_dir = root_pattern("mod.ts")
+-- }
